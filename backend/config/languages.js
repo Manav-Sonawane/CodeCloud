b10a73115439f8206e882adbb66a0dc562ad3e28
Code_with_Manav <codewithmanav70@gmail.com>
2025-09-06 00:36:54 +0530

    extended language support

---
+++ backend/config/languages.js
@@ -0,0 +1,6 @@
+export const languageConfig = {
+  python3: { versionIndex: "3", mode: "python" },
+  cpp17: { versionIndex: "0", mode: "text/x-c++src" },
+  java: { versionIndex: "4", mode: "text/x-java" },
+  nodejs: { versionIndex: "3", mode: "javascript" },
+};
